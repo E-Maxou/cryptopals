4154a37b8d4d810daa19e1eb1d90b55dce0edb33
Maxime <maxime.elkael@wanadoo.fr>
2018-12-27 23:18:56 +0100

    did challenge 9

---
+++ challenge9.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func pkcs7(str string, length int) []byte {
+	bytes := []byte(str)
+
+	pkcs7 := []byte{0x04}
+	fmt.Println(length)
+	limit := length - len(bytes)
+	for i := 0; i < limit; i++ {
+		bytes = append(bytes, pkcs7...)
+	}
+	return bytes
+}
+
+func main() {
+	a := pkcs7("YELLOW SUBMARINE", 20)
+	fmt.Println(a, string(a))
+}
