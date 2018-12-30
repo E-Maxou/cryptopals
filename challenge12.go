6609e18065f48b1e7f33a9152bdab87c200201e4
Maxime <maxime.elkael@wanadoo.fr>
2018-12-30 12:13:11 +0100

    added padding

--- challenge12.go
+++ challenge12.go
@@ -3,10 +3,16 @@ package main
 // compiles with go run challene12.go challenge11.go
 
 import "fmt"
+import b64 "encoding/base64"
+import hex "encoding/hex"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		encrypted := encryptRandom("414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141")
-		fmt.Println(oracleMode(encrypted))
-	}
+	b64string := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
+		"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
+		"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg" +
+		"YnkK"
+	byteStr, _ := b64.StdEncoding.DecodeString(b64string)
+	hexStr := hex.EncodeToString(byteStr)
+	encrypted := encryptRandom("414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141" + hexStr)
+	fmt.Println(oracleMode(encrypted))
 }
