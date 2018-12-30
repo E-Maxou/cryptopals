98d15961fd650303cc88b696af55b62466d777d4
Maxime <maxime.elkael@wanadoo.fr>
2018-12-30 13:46:13 +0100

    challenge 12 solved ! yay my first real world crypto attack, im hyped

--- challenge12.go
+++ challenge12.go
@@ -6,13 +6,76 @@ import "fmt"
 import b64 "encoding/base64"
 import hex "encoding/hex"
 
-func main() {
-	b64string := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
+func truncatedHexUnk(nbToRm int) string {
+	b64Unk := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
 		"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
 		"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg" +
 		"YnkK"
-	byteStr, _ := b64.StdEncoding.DecodeString(b64string)
-	hexStr := hex.EncodeToString(byteStr)
-	encrypted := encryptRandom("414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141414141" + hexStr)
-	fmt.Println(oracleMode(encrypted))
+	byteUnk, _ := b64.StdEncoding.DecodeString(b64Unk)
+	hexUnk := hex.EncodeToString(byteUnk)
+	if nbToRm*2 > len(hexUnk) {
+		return ""
+	}
+	return hexUnk[nbToRm*2:]
+}
+
+func encryptConcatenation(myHexString string, hexUnk string, key []byte) []byte {
+	concat, _ := hex.DecodeString(myHexString + hexUnk)
+	return ecbEncrypt(key, concat)
+}
+
+func discoverKeysize() int {
+	for i := 1; i < 65; i++ {
+		myString := ""
+		for j := 0; j < i; j++ {
+			myString += "FF"
+		}
+		key := randomKey()
+		encrypted := encryptConcatenation(myString, truncatedHexUnk(0), key)
+
+		if oracleMode(encrypted) == "ecb" {
+			// dividing by 2 because we fed exactly two blocks here (we return the first time we meet two identical blocks)
+			return i / 2
+		}
+	}
+	return -1
+}
+
+func makeByteMap(key []byte) map[string]int {
+	m := make(map[string]int)
+	beginning := ""
+	for i := 0; i < discoverKeysize()-1; i++ {
+		beginning += "41"
+	}
+	for i := 0; i < 256; i++ {
+		blockHex := beginning
+		lastByte := fmt.Sprintf("%x", i)
+		if len(lastByte) == 0 {
+			lastByte = "00"
+		}
+		if len(lastByte) == 1 {
+			lastByte = "0" + lastByte
+		}
+		blockHex += lastByte
+		block, _ := hex.DecodeString(blockHex)
+		encrypted := ecbEncrypt(key, block)
+		encryptedHex := hex.EncodeToString(encrypted)
+		m[encryptedHex] = i
+	}
+	return m
+}
+
+func main() {
+	key := randomKey()
+	m := makeByteMap(key)
+	cat := ""
+	for i := 0; i < discoverKeysize()-1; i++ {
+		cat += "41"
+	}
+	result := make([]byte, 0)
+	for i := 0; truncatedHexUnk(i) != ""; i++ {
+		encrypted := encryptConcatenation(cat, truncatedHexUnk(i), key)
+		result = append(result, byte(m[hex.EncodeToString(encrypted[:16])]))
+	}
+	fmt.Print("Challenge 12 : ", string(result))
 }
